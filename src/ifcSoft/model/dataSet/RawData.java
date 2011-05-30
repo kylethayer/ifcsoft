c7693b0717b64eb1dac23b24966e63dc8e9a100a
Tony <Tony@kibeth>
2011-05-30 13:58:22 -0400

    Replaced all the StringTokenizer Objects (which are deprecated and less efficient) with String arrays using String.split().

--- src/ifcSoft/model/dataSet/RawData.java
+++ src/ifcSoft/model/dataSet/RawData.java
@@ -26,12 +26,12 @@ import java.io.DataInputStream;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.Reader;
-import java.net.HttpURLConnection;
+//import java.io.Reader;
+//import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLConnection;
 import java.util.LinkedList;
-import java.util.StringTokenizer;
+//import java.util.StringTokenizer;
 
 /**
  *
